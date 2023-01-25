597c7309c616916fcb124ce9f30d898900d7779d
Codey V <codeyvanourkerk@avixi.com>
2023-01-25 12:58:58 -0700

    Add project files.

---
+++ OAuth2Client/OAuth2ClientForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿#pragma warning disable
+namespace OAuth2Client
+{
+    partial class OAuth2ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OAuth2ClientForm));
+            this.authWindow = new Microsoft.Toolkit.Forms.UI.Controls.WebViewCompatible();
+            this.SuspendLayout();
+            // 
+            // authWindow
+            // 
+            this.authWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.authWindow.Location = new System.Drawing.Point(0, 0);
+            this.authWindow.Name = "authWindow";
+            this.authWindow.Size = new System.Drawing.Size(800, 450);
+            this.authWindow.TabIndex = 0;
+            // 
+            // OAuth2ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.authWindow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OAuth2ClientForm";
+            this.Text = "Authorization Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Toolkit.Forms.UI.Controls.WebViewCompatible authWindow;
+    }
+}
+
---
+++ OAuth2Client/OAuth2ClientForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAuth2Client
+{
+    public partial class OAuth2ClientForm : Form
+    {
+        static readonly HttpClient client = new HttpClient();
+        string AuthorizationURL;
+
+        public OAuth2ClientForm() { InitializeComponent(); }
+
+        public OAuth2ClientForm(string authorizationURL) : base()
+        {
+            AuthorizationURL = authorizationURL;
+        }
+    }
+}
